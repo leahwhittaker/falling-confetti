5f97b9e3722ba74efd874bee3d9c5a7004976de5
Leah Whittaker <lwhittaker@conversantmedia.com>
2015-11-21 14:40:42 -0600

    added back animation

--- js/FallingConfetti.js
+++ js/FallingConfetti.js
@@ -34,6 +34,45 @@ define(function (require) {
         magPlanes.camera = camera;
         magPlanes.init();
 
+        // animate the planes like confetti
+        for (var i = 0; i < magPlanes.planes.length; i++) {
+            var plane = magPlanes.planes[i];
+            var delay = Math.random() * 2.5;
+            if (delay > 2.45) {
+                delay += 1.5 * Math.random();
+            }
+            TweenMax.from(plane.position, 4.5, {y: 200, z: 410, ease: "Power3.easeIn", delay: delay});
+            TweenMax.from(plane.rotation, 4.5, {
+                x: Math.random() * 100,
+                y: Math.random() * 100,
+                z: Math.random() * 100,
+                ease: "Linear.easeNone",
+                delay: delay,
+                onCompleteParams: [plane],
+                onComplete: function (p) {
+
+
+                    if (Math.random() < 0.3) {
+                        var rando = (Math.random() * 0.15) + 0.05;
+                        TweenMax.to(p.position, rando, {
+                            z: Math.random() * 15,
+                            ease: "Sine.easeOut",
+                            yoyo: true,
+                            repeat: 1
+                        });
+                        TweenMax.to(p.rotation, rando * 2, {x: 180 * (Math.PI / 180)});
+                    } else {
+                        var addlRotation = p.rotation.z + ((Math.random() * 90 - 90) * (Math.PI / 180));
+                        TweenMax.to(p.rotation, Math.random() * 0.5, {z: addlRotation});
+                        TweenMax.to(p.position, Math.random() * 0.5, {
+                            x: "+=" + String(Math.random * 5),
+                            y: "-=" + String(Math.random() * 5)
+                        })
+                    }
+                }
+            });
+        }
+
         function render() {
 
             requestAnimationFrame(render);
